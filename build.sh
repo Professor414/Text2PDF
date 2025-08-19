7bef29e0b4f62d96c981218a1ddadb2ad439545b
Professor414 <nidesmie@gmail.com>
2025-08-19 22:44:09 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,4 @@
 # exit on error
 set -o errexit
 
-# 1. ដំឡើង Python libraries ពី requirements.txt
 pip install -r requirements.txt
-
-# 2. ដំឡើង Dependencies របស់ WeasyPrint និង Font ខ្មែរ
-# នេះជាផ្នែកដែលដោះស្រាយបញ្ហា Font!
-apt-get update && apt-get install -y libpango-1.0-0 libpangoft2-1.0-0 libcairo2 fonts-khmeros
